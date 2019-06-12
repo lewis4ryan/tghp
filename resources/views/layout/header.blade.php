fab033305ba3ea4162561f9669776c3c15b6ceb9
thiengoan <tran_thien_ngoan@management-partners.co.jp>
2019-06-12 12:57:22 +0700

    add layout

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,93 @@
+<div class="container">
+        <header>
+            <div class="d-flex flex-row bd-highlight mb-3 topbar">
+                <div class="p-2 bd-highlight">
+                    <h1> 人材派遣、転職支援ならトラストグロース</h1>
+                    <a href="/index.php"><img src="/images/header_logo.png" alt="トラストグロース" /></a>
+                </div>
+                <div class="p-2 bd-highlight">
+                    <a href="#" class="img-top"><img src="/images/top1.png" alt="スタッフ様向けシステムログイン" /> </a>
+                </div>
+                <div class="p-2 bd-highlight">
+                    <a href="#" class="img-top"><img src="/images/top2.png" alt="企業様向けシステムログイン" /></a>
+                </div>
+                <div class="p-2 bd-highlight">
+                    <a href="#" class="img-top"><img src="/images/top3.png" alt="お仕事紹介" /></a>
+                </div>
+                <div class="p-2 bd-highlight">
+                    <a href="/index.php" class="img-top"><img src="/images/top4.png" alt="電話番号" /></a>
+                </div>
+            </div>
+
+            <div class="navbar navbar-expand-lg bsnav bsnav-light container">
+                <a class="navbar-brand logo" href="/index.php"><img src="/images/header_logo.png" alt="トラストグロース" /></a>
+                <button class="navbar-toggler toggler-spring mgr-top"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse ">
+                    <ul class="navbar-nav navbar-mobile mr-0">
+                        <li class="nav-item dropdown"><a class="nav-link" href="#">サービス紹介<i class="caret"></i></a>
+                            <ul class="navbar-nav">
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/welfare/">医療・福祉</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/engineer/">ＩＴエンジニア</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/synthesis/">総合人材派遣</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="#"> 新卒就活サポート</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown"><a class="nav-link" href="#">お仕事検索 <i class="caret"></i></a>
+                            <ul class="navbar-nav">
+                                <li class="nav-item sub-menu"><a class="nav-link" href="#">医療・福祉</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/search/">総合人材派遣</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown"><a class="nav-link" href="#">求職者の方 <i class="caret"></i></a>
+                            <ul class="navbar-nav">
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/beginner/">初めての方へ</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/dispatch/">派遣のしくみ</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/flow/">ご登録から就業まで</a></li>
+                                <div class="dropdown-divider"></div>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/interview/">利用者インタビュー</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/faq">よくあるご質問</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/resume/">履歴書作成</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown"><a class="nav-link" href="#">企業のご担当の方 <i class="caret"></i></a>
+                            <ul class="navbar-nav">
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/client/service/">サービス紹介</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/client/flow/">就業開始まで</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/client/scout/">スカウト機能</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/client/faq/">よくあるご質問</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-item sub-menu"><a class="nav-link" href="/registration/">企業登録</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="/company/">会社概要</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/inquiry/">お問い合わせ</a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+
+
+            <div class="bsnav-mobile">
+                <div class="bsnav-mobile-overlay"></div>
+                <div class="navbar"></div>
+            </div>
+          </header>
+    </div>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+
+      </div>
+    </div>
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>株式会社トラストグロース｜人材派遣・人材紹介・新卒採用・転職支援会社</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{url('/css/bootstrap.min.css')}}" />
+        <link rel="stylesheet" href="{{url('/css/menu.css')}}" />
+        <link rel="stylesheet" href="{{url('/css/line-awesome.min.css')}}">
+        <link rel="stylesheet" href="{{url('/css/bsnav.min.css')}}">
+        <link rel="stylesheet" href="{{url('/css/examples.min.css')}}">
+        <link rel="stylesheet" href="{{url('/css/font.css')}}"  />
+        <link rel="stylesheet" href="{{url('/css/choose.css')}}" />
+        <link rel="stylesheet" href="{{url('/css/footer.css')}}" />
+        <link rel="stylesheet" href="{{url('/css/new_layout.css')}}" >
+        <link rel="stylesheet" href="{{url('/css/media.css')}}">
+        <link rel="stylesheet" href="{{url('/css/font.css')}}">
+    </head>
+    <body>
+        @include('layout.header')
+        @section('content') 
+        @show
+        @include('layout.footer')
+
+        <script src="{{url('/js/jquery-2.2.4.min.js')}}" ></script>
+        <script src="{{url('/js/bsnav.min.js')}}"></script> 
+        <script src="{{url('/js/my-button-toppage.js')}}"></script>
+        <script src="{{url('/js/new-js/popper.min.js')}}"></script>
+        <script src="{{url('/js/new-js/bootstrap.min.js')}}"></script>
+        <script src="{{url('/js/fSelect.js')}}"></script>
+    </body>
+</html>
